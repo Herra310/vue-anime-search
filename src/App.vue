f410802c4d583930f58ffbe3a24d40708fcea074
JORGE ARATH HERRERA ROMERO <17171387@itculiacan.edu.mx>
2022-09-19 15:34:35 -0600

    terminado

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,142 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <h1>El buscador de <strong>Animes</strong></h1>
+
+      <form class="search-box" @submit.prevent="handleSearch">
+        <input
+          type="search"
+          class="search-field"
+          placeholder="Buscar anime... "
+          required
+          v-model="search_query"
+        />
+      </form>
+    </header>
+
+    <main>
+      <div class="cards">
+        <Card v-for="anime in animelist" :key="anime.mal_id"
+        :anime="anime" />
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+import Card from "./components/Card";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    Card,
+  },
+  setup() {
+    const search_query = ref("");
+    const animelist = ref([]);
+
+    const handleSearch = async () => {
+      animelist.value = await fetch(
+        `https://api.jikan.moe/v3/search/anime?q=${search_query.value}`
+      )
+        .then((res) => res.json())
+        .then((data) => data.results);
+
+      search_query.value = "";
+    };
+    return {
+      Card,
+      search_query,
+      animelist,
+      handleSearch,
+    };
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+  font-family: sans-serif;
+}
+
+a {
+  text-decoration: none;
+}
+
+header {
+  padding-top: 50px;
+  padding-bottom: 50px;
+
+  h1 {
+    color: #888;
+    font-size: 42px;
+    font-weight: 400;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+
+    strong {
+      color: #313131;
+    }
+
+    &:hover {
+      color: #313131;
+    }
+  }
+
+  .search-box {
+    display: flex;
+    justify-content: center;
+    padding-left: 30px;
+    padding-right: 30px;
+
+    .search-field {
+      appearance: none;
+      background: none;
+      border: none;
+      outline: none;
+
+      background: #f3f3f3;
+      box-shadow: 0px 4px 8px rgba($color: #000000, $alpha: 0.15);
+
+      display: block;
+      width: 100%;
+      max-width: 600px;
+      padding: 15px;
+      border-radius: 8px;
+
+      color: #313131;
+      font-size: 20px;
+
+      transition: 0.4s;
+
+      &::placeholder {
+        color: #aaa;
+      }
+
+      &:focus,
+      &:valid {
+        color: #fff;
+        background-color: #313131;
+        box-shadow: 0px 0px 0px rgba($color: #000000, $alpha: 0.15);
+      }
+    }
+  }
+}
+
+main {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding-left: 30px;
+  padding-right: 30px;
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0 -8px;
+  }
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card">
+      <a :href="anime.url" target="_blank">
+          <img :src="anime.image_url" />
+          <h3> {{anime.title}}</h3>
+      </a>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['anime']
+}
+</script>
+
+<style lang='scss' scoped>
+.card{
+     flex: 1 1 33%;
+     width: 33%;
+     max-width: 33%;
+     padding: 0 8px;
+     margin: 0 0 16px 0;
+
+     img{
+         width: 100%;
+         height: 300px;
+         object-fit: cover;
+         border-radius: 14px;
+         box-shadow: 0 4px 8px rgba($color: #000000, $alpha: 0.15);
+
+         transition:0.4s;
+     }
+
+     h3{
+         padding: 16px;
+         color: #313131;
+         transition: 0.5s;
+     }
+
+     &:hover{
+         img{
+             transform: scale(1.05);
+         }
+
+         h3{
+             color: #24C444;
+         }
+     }
+}
+</style>
